1b3eaea2629800abf27af777571baba94c0a45c3
pavan <pavan@vmone.myguest.virtualbox.org>
2023-08-12 12:33:23 +0530

    Added policy generator

---
+++ policy_generator/go.mod
@@ -0,0 +1,5 @@
+module cloudsweep/policy_generator
+
+go 1.20
+
+require github.com/itchyny/json2yaml v0.1.4 // indirect
---
+++ policy_generator/json_to_yaml.go
@@ -0,0 +1,45 @@
+package policy_generator
+
+import (
+    "os"
+    "strings"
+    "github.com/itchyny/json2yaml"
+    "sync"
+)
+var (
+    mutex sync.Mutex
+)
+
+// Converts the json file and returns the content into an file
+//
+// Converts the json file and returns the content into an file.
+// It returns error when an error is encountered.
+func convertJsonToYaml(json string) (string, error) {
+    var yaml strings.Builder
+    if err := json2yaml.Convert(&yaml, strings.NewReader(json)); err != nil {
+        return "", err
+    }
+    return yaml.String(), nil
+}
+
+// Converts the json file and writes the content into an file
+//
+// Converts the json file and writes the content into an file.
+// It returns error when an error is encountered.
+func convertJsonToYamlAndWriteToFile(json string, path string) (error) {
+    var yaml strings.Builder
+    if err := json2yaml.Convert(&yaml, strings.NewReader(json)); err != nil {
+        return err
+    }
+    mutex.Lock()
+    defer mutex.Unlock()
+    if file, err := os.Create(path); err != nil {
+        return err
+    } else {
+        defer file.Close()
+        if _, err := file.Write([]byte(yaml.String())); err !=nil {
+            return err
+        }
+        return nil
+    }
+}
